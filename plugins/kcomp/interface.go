aab13c591b28c652fc4dd5493f7dd8731af6e5ad
Phathdt <phathdt379@gmail.com>
2024-08-28 16:15:18 +0700

    add consumer comp

--- plugins/kcomp/interface.go
+++ plugins/kcomp/interface.go
@@ -1,5 +1,14 @@
 package kcomp
 
+type Message struct {
+	Key     []byte
+	Payload []byte
+}
+
 type KProducer interface {
 	Publish(topic string, key string, value interface{}) error
+}
+
+type KConsumer interface {
+	Subscribe(topic string, handlerFunc func(msg *Message) error)
 }
